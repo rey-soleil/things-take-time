ad339db621806bfc11238f7b1d9640b09d11149d
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-04-12 17:49:46 -0700

    feat: implement time heatmap

--- src/app/insights/page.tsx
+++ src/app/insights/page.tsx
@@ -1,103 +1,49 @@
 "use client";
 
-import _ from "lodash";
-import { useEffect, useState } from "react";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { useState } from "react";
 import HeatMap from "../components/HeatMap";
 
-type Event = { id: string; summary: string };
-
-// How many 15-minute intervals there are in a day
-const NUM_INTERVALS = 4 * 24;
-
-/* Cluster related event summaries,
- * e.g. "meditate ☀️", "meditate" => "meditate"
+/*
+ * The Insights page allows the user to see when they tend to do each activity on their calendar.
+ * Users select an activity from a dropdown, and the page displays a heatmap of when they tend to do that activity.
  */
-function clean(summary: string) {
-  if (summary === undefined) return undefined;
-  if (summary.startsWith("meditate")) return "meditate";
-  if (summary === "gym" || summary.startsWith("exercise")) return "exercise";
-  if (summary.startsWith("read")) return "read";
-  if (summary.startsWith("journal")) return "journal";
-  if (summary.includes("dinner")) return "dinner";
-  if (summary.startsWith("code")) return "code";
-  if (summary.includes("piano")) return "piano";
-  if (summary.includes("walk")) return "walk";
-  if (summary.includes(" w ")) return "social";
-  if (summary.startsWith("climb")) return "climb";
-  return summary;
-}
-
-function getMinutesSinceStartOfDay(dateTime) {
-  const startOfDay = new Date(
-    dateTime.getFullYear(),
-    dateTime.getMonth(),
-    dateTime.getDate()
-  );
-  const diffMilliseconds = dateTime.getTime() - startOfDay.getTime();
-  const diffMinutes = Math.floor(diffMilliseconds / 60000);
-  return diffMinutes;
-}
-
-function convertToHeatMap(events: Event[]) {
-  const heatMap = new Array(NUM_INTERVALS).fill(0);
-  events.forEach((event) => {
-    const startInterval = Math.floor(
-      getMinutesSinceStartOfDay(new Date(_.get(event, "start.dateTime"))) / 15
-    );
-    const endInterval = Math.floor(
-      getMinutesSinceStartOfDay(new Date(_.get(event, "end.dateTime"))) / 15
-    );
-    for (let i = startInterval; i < endInterval; i++) {
-      heatMap[i] += 1;
-    }
-  });
-  return heatMap;
-}
-
 export default function Insights() {
-  const [events, setEvents] = useState<Event[]>();
-  const [groupedEvents, setGroupedEvents] = useState<[string, Event[]][]>();
+  const [activity, setActivity] = useState<string>("");
 
-  function groupEvents(events: Event[]) {
-    const groupedEvents = _(events)
-      .filter((event) => event.summary !== undefined)
-      .groupBy((event) => clean(event.summary))
-      .toPairs()
-      .orderBy((pair) => pair[1].length, "desc")
-      .valueOf();
-    setGroupedEvents(groupedEvents);
-  }
+  // TODO: After the UI is implemented, think of how to efficiently
+  // 1. Get events from /api/insights
+  // 2. Group events by activity
+  // 3. Convert each group of events to a heatmap
+  const heatmaps = JSON.parse(window.localStorage.getItem("heatmaps") || "{}");
 
-  async function loadEvents() {
-    const events = await fetch("api/insights").then((res) => res.json());
-    groupEvents(events);
-    return events;
-  }
+  // An array of activities sorted by descending frequency
+  const activities: string[] = JSON.parse(
+    window.localStorage.getItem("activities") || "[]"
+  );
 
-  useEffect(() => {
-    loadEvents().then((events) => setEvents(events));
-  }, []);
-
-  console.log({ events, groupedEvents });
-
-  if (!events) return <div>Loading...</div>;
+  console.log({ heatmaps, activities, activity });
 
   return (
-    <div>
-      {/* {events.map((event) => (
-        <div key={event.id}>{`${event.summary} ${_.get(
-          event,
-          "start.dateTime"
-        )} ${_.get(event, "end.dateTime")}`}</div>
-      ))} */}
-      {groupedEvents &&
-        groupedEvents.length > 0 &&
-        groupedEvents.map(([summary, events]) => (
-          <div key={summary}>
-            <h2>{summary}</h2>
-            <HeatMap heatmap={convertToHeatMap(events)} />
-          </div>
-        ))}
+    <div className="flex flex-col items-center m-6">
+      <div className="items-center w-2/4">
+        <FormControl fullWidth>
+          <InputLabel>Activity</InputLabel>
+          <Select
+            value={activity}
+            label="Activity"
+            onChange={({ target }) => setActivity(target.value as string)}
+            sx={{ backgroundColor: "white", margin: "1rem 0" }}
+          >
+            {activities.map((activity) => (
+              <MenuItem key={activity} value={activity}>
+                {activity}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        {activity !== "" && <HeatMap heatmap={heatmaps[activity]} />}
+      </div>
     </div>
   );
 }
