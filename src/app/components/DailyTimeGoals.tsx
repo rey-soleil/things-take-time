1bec07eef6216c46bca5ad98df921865d605c096
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-05-09 12:09:45 -0700

    feat(time-goals): print time logged today

--- src/app/components/DailyTimeGoals.tsx
+++ src/app/components/DailyTimeGoals.tsx
@@ -1,12 +1,25 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { GCalEvent } from "../insights/page";
+import { formatAsString, getTimeLoggedToday } from "../utils";
 
 type DailyTimeGoalsProps = {
   events: GCalEvent[];
 };
 
 export default function DailyTimeGoals({ events }: DailyTimeGoalsProps) {
-  console.log("in DailyTimeGoals.tsx, events: ", events);
-  return <></>;
+  const [timeLoggedToday, setTimeLoggedToday] = useState(0);
+
+  useEffect(() => {
+    setTimeLoggedToday(getTimeLoggedToday(events));
+  }, [events]);
+
+  return (
+    <>
+      <div className="text-6xl">
+        You&apos;ve logged {formatAsString(timeLoggedToday)} today
+      </div>
+    </>
+  );
 }
