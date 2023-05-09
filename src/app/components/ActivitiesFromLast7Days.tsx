d723e597708dac532a46a5ce9cae4136365eaf03
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-05-09 16:24:02 -0700

    style(time-goals): add comments & clean up var names

--- src/app/components/ActivitiesFromLast7Days.tsx
+++ src/app/components/ActivitiesFromLast7Days.tsx
@@ -4,6 +4,7 @@ import {
   BarChart,
   CartesianGrid,
   Legend,
+  ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
@@ -17,13 +18,14 @@ const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
 
 const DEFAULT_NUM_DAYS = 7;
 
-export default function StackedBarChart({
+export default function ActivitiesFromLast7Days({
   taskDurationByDate,
 }: StackedBarCharProps) {
   const [data, setData] = useState<any[]>([]);
   const [mostCommonTasks, setMostCommonTasks] = useState<string[]>([]);
 
   useEffect(() => {
+    // durationByTask: array of (activity, duration) pairs
     const durationByTask = {};
     Object.keys(taskDurationByDate).forEach((date) => {
       Object.keys(taskDurationByDate[date]).forEach((task) => {
@@ -31,14 +33,17 @@ export default function StackedBarChart({
           ((durationByTask as any)[task] || 0) + taskDurationByDate[date][task];
       });
     });
+
+    // mostCommonTasks: the 5 tasks with the most cumulative duration
     const mostCommonTasks = Object.entries(durationByTask)
       .sort((a, b) => (a[1] as number) - (b[1] as number))
       .reverse()
       .map((a) => a[0])
       .slice(0, Math.min(5, Object.keys(durationByTask).length));
     setMostCommonTasks(mostCommonTasks);
-    console.log({ taskDurationByDate, durationByTask, mostCommonTasks });
 
+    // data: array of bar chart data for rechart
+    // eg. data[0] = { "name": "Wed May 03 2023", "meditate": 66, "read": 91, "walk": 97}
     const data = Array(DEFAULT_NUM_DAYS);
     for (let i = 0; i < DEFAULT_NUM_DAYS; i++) {
       const date = new Date(
@@ -53,32 +58,35 @@ export default function StackedBarChart({
     }
     data.reverse();
     setData(data);
-
-    console.log({ data });
   }, [taskDurationByDate]);
 
   return (
-    <>
-      {/* <ResponsiveContainer width={500} height={300}> */}
-      <BarChart width={1000} height={300} data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip
-          formatter={(value) =>
-            (value as number) > 60
-              ? `${Math.floor((value as number) / 60)}h ${
-                  (value as number) % 60
-                }m`
-              : `${value}m`
-          }
-        />
-        <Legend />
-        {mostCommonTasks.map((task, i) => (
-          <Bar key={task} dataKey={task} stackId="a" fill={colors[i]} />
-        ))}
-      </BarChart>
-      {/* </ResponsiveContainer> */}
-    </>
+    <div className="h-96 max-w-full">
+      <ResponsiveContainer
+        minWidth={350}
+        minHeight={100}
+        width="100%"
+        height="100%"
+      >
+        <BarChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip
+            formatter={(value) =>
+              (value as number) > 60
+                ? `${Math.floor((value as number) / 60)}h ${
+                    (value as number) % 60
+                  }m`
+                : `${value}m`
+            }
+          />
+          <Legend />
+          {mostCommonTasks.map((task, i) => (
+            <Bar key={task} dataKey={task} stackId="a" fill={colors[i]} />
+          ))}
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
