2de14c0b2f16d19b8ba248e87b77491b8364f633
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-05-09 12:33:30 -0700

    feat(time-goals): create map taskDurationByDate

--- src/app/components/DailyTimeGoals.tsx
+++ src/app/components/DailyTimeGoals.tsx
@@ -2,24 +2,39 @@
 
 import { useEffect, useState } from "react";
 import { GCalEvent } from "../insights/page";
-import { formatAsString, getTimeLoggedToday } from "../utils";
+import {
+  formatAsString,
+  getTaskDurationByDate,
+  getTimeLoggedToday,
+} from "../utils";
 
 type DailyTimeGoalsProps = {
   events: GCalEvent[];
 };
 
 export default function DailyTimeGoals({ events }: DailyTimeGoalsProps) {
+  // This is the total time logged today, in milliseconds.
   const [timeLoggedToday, setTimeLoggedToday] = useState(0);
+
+  const [taskDurationByDate, setTaskDurationByDate] = useState({});
 
   useEffect(() => {
     setTimeLoggedToday(getTimeLoggedToday(events));
+    setTaskDurationByDate(getTaskDurationByDate(events));
   }, [events]);
+
+  console.log({
+    taskDurationByDate,
+    today: new Date().toDateString(),
+    values: taskDurationByDate[new Date().toDateString()],
+  });
 
   return (
     <>
       <div className="text-6xl">
         You&apos;ve logged {formatAsString(timeLoggedToday)} today
       </div>
+      <div>{JSON.stringify(taskDurationByDate[new Date().toDateString()])}</div>
     </>
   );
 }
