07f15419d9042272f50d7384e291fc76084ab7a5
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-05-12 20:46:56 -0700

    feat(insights): implement TaskChart

--- src/app/components/insights/TaskSelector.tsx
+++ src/app/components/insights/TaskSelector.tsx
@@ -1,47 +1,58 @@
-// "use client";
+"use client";
 
-// import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
+import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
 
-// export default function TaskSelector() {
-//   return (
-//     <div>
-//       <FormGroup>
-//         {tasks.map((task) => (
-//           <FormControlLabel
-//             key={task}
-//             control={<Checkbox defaultChecked />}
-//             label={task}
-//             checked={selectedTasks.has(task)}
-//             onChange={() => {
-//               if (selectedTasks.has(task)) {
-//                 selectedTasks.delete(task);
-//               } else {
-//                 selectedTasks.add(task);
-//               }
-//               setSelectedTasks(new Set(selectedTasks));
-//             }}
-//           />
-//         ))}
-//         <hr></hr>
-//         <FormControlLabel
-//           key={"form_control_deselect"}
-//           control={<Checkbox />}
-//           label="deselect all"
-//           checked={selectedTasks.size === 0}
-//           onChange={() => {
-//             setSelectedTasks(new Set());
-//           }}
-//         />
-//         <FormControlLabel
-//           key={"form_control_select"}
-//           control={<Checkbox />}
-//           label="select all"
-//           checked={selectedTasks.size === tasks.length}
-//           onChange={() => {
-//             setSelectedTasks(new Set(tasks));
-//           }}
-//         />
-//       </FormGroup>
-//     </div>
-//   );
-// }
+type TaskSelectorProps = {
+  taskLabels: string[];
+  selectedTasks: Set<string>;
+  setSelectedTasks: (tasks: Set<string>) => void;
+};
+
+export default function TaskSelector({
+  taskLabels,
+  selectedTasks,
+  setSelectedTasks,
+}: TaskSelectorProps) {
+
+    return (
+    <div>
+      <FormGroup>
+        {taskLabels.map((task) => (
+          <FormControlLabel
+            key={task}
+            control={<Checkbox defaultChecked />}
+            label={task}
+            checked={selectedTasks.has(task)}
+            onChange={() => {
+              if (selectedTasks.has(task)) {
+                selectedTasks.delete(task);
+              } else {
+                selectedTasks.add(task);
+              }
+              setSelectedTasks(new Set(selectedTasks));
+            }}
+          />
+        ))}
+        <hr></hr>
+        <FormControlLabel
+          key={"form_control_deselect"}
+          control={<Checkbox />}
+          label="deselect all"
+          checked={selectedTasks.size === 0}
+          onChange={() => {
+            setSelectedTasks(new Set());
+          }}
+        />
+        <FormControlLabel
+          key={"form_control_select"}
+          control={<Checkbox />}
+          label="select all"
+          checked={selectedTasks.size === taskLabels.length}
+          onChange={() => {
+            setSelectedTasks(new Set(taskLabels));
+          }}
+        />
+      </FormGroup>
+    </div>
+  );
+}
--- src/app/components/insights/TimeLogged.tsx
+++ src/app/components/insights/TimeLogged.tsx
@@ -3,16 +3,21 @@ import { useMemo } from "react";
 
 type TimeLoggedProps = {
   clusteredTasks: ClusteredTasks;
+  selectedTasks: Set<string>;
 };
 
-export default function TimeLogged({ clusteredTasks }: TimeLoggedProps) {
+export default function TimeLogged({
+  clusteredTasks,
+  selectedTasks,
+}: TimeLoggedProps) {
+  // TODO: make timeLogged responsive to selectedTasks
   const timeLogged = useMemo(
     () =>
-      Object.keys(clusteredTasks).reduce(
-        (totalTime, task) => totalTime + clusteredTasks[task].duration,
+      Array.from(selectedTasks).reduce(
+        (totalTime, task) => totalTime + clusteredTasks[task]?.duration,
         0
       ),
-    [clusteredTasks]
+    [clusteredTasks, selectedTasks]
   );
 
   return <div className="text-7xl">{formatAsHourAndMinutes(timeLogged)}</div>;
