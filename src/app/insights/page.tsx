e6fed33bc3866cac10d0fdb11edbae7eb38bb495
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-05-12 20:55:35 -0700

    style(insights): a few comments

--- src/app/insights/page.tsx
+++ src/app/insights/page.tsx
@@ -61,6 +61,7 @@ export default function Insights() {
     loadTasks();
   }, [selectedNumDays]);
 
+  // TODO: implement a loading spinner
   if (isLoading) return <div>Loading...</div>;
 
   return (
