fb8b8d71bd084c345006afc201ce25acf17e5f9a
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-05-22 14:14:39 -0700

    refactor(api): move OAuth code outside of POST

--- src/app/api/event/route.ts
+++ src/app/api/event/route.ts
@@ -5,11 +5,9 @@ const oAuth2Client = new OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET
 );
-
 oAuth2Client.setCredentials({
   refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
 });
-
 const calendar = google.calendar({ version: "v3", auth: oAuth2Client });
 
 export async function POST(request: Request) {
