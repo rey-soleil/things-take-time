51d8f2b813ad88146fe0ef1131f80695c6ddaf80
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-06-16 15:11:32 -0700

    refactor: aliased imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
+import Stopwatch from "components/Stopwatch";
 import { useSession } from "next-auth/react";
-import Stopwatch from "../../components/Stopwatch";
 
 export default function Home() {
   const { data: session } = useSession({ required: true });
 
   // TODO: save #F2F2F2 as a CSS variable
-  return <main className="h-screen w-screen bg-[#F2F2F2]">
-    <Stopwatch/>
-  </main>;
+  return (
+    <main className="h-screen w-screen bg-[#F2F2F2]">
+      <Stopwatch />
+    </main>
+  );
 }
