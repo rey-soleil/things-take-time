c71170730ba221d0dfcca49f5e90cc1dd2a76f6c
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-06-16 16:48:25 -0700

    feat(timeline): add Timeline component

--- components/Stopwatch.tsx
+++ components/Stopwatch.tsx
@@ -1,24 +1,10 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
 import { formatAsHHMMSS } from "utils/time";
 
-export default function Stopwatch({
-  startTime,
-  setIntervalId,
-}: {
-  startTime: number | undefined;
-  setIntervalId: (intervalId: NodeJS.Timer) => void;
-}) {
-  const [milliseconds, setMilliseconds] = useState(0);
-
-  useEffect(() => {
-    if (!startTime) return;
-    const intervalId = setInterval(() => {
-      setMilliseconds(Date.now() - startTime);
-    }, 1000);
-    setIntervalId(intervalId);
-  }, [startTime]);
-
+export default function Stopwatch({ milliseconds }: { milliseconds: number }) {
   // TODO: add style
-  return <div className="text-7xl">{formatAsHHMMSS(milliseconds)}</div>;
+  return (
+    <div className="font-mono text-8xl font-medium">
+      {formatAsHHMMSS(milliseconds)}
+    </div>
+  );
 }
