e21200057c376d434d634d9415338f280c49c963
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-06-15 18:31:29 -0700

    refactor(stopwatch): create CuteButton component

---
+++ components/CuteButton.tsx
@@ -0,0 +1,17 @@
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+type CuteButtonProps = {
+  onClick: () => void;
+  icon: IconDefinition;
+  text: string;
+};
+
+export default function CuteButton({ onClick, icon, text }: CuteButtonProps) {
+  return (
+    <button onClick={onClick} className="m-5 flex flex-col items-center">
+      <FontAwesomeIcon icon={icon} className="text-7xl" />
+      <span className="m-1 text-base">{text}</span>
+    </button>
+  );
+}
