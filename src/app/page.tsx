c76eae5112aba937c2dd71bf9b22afac4d57e14a
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-06-16 15:02:42 -0700

    refactor(home): use gray background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,5 +5,6 @@ import { useSession } from "next-auth/react";
 export default function Home() {
   const { data: session } = useSession({ required: true });
 
-  return <main></main>;
+  // TODO: save #F2F2F2 as a CSS variable
+  return <main className="h-screen w-screen bg-[#F2F2F2]"></main>;
 }
