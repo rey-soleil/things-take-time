2191d4bb355cd92c2553da89791daa8864949b3e
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-06-20 11:50:05 -0700

    fix(route): return 200 status code

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 import { Button } from "@mui/material";
 import { signIn } from "next-auth/react";
-import { useSearchParams } from "next/navigation";
 
 export default function SignIn() {
-  const searchParams = useSearchParams();
-  const callbackUrl = searchParams.get("callbackUrl") || undefined;
-
   return (
     <div className="flex h-screen items-center justify-center">
-      <Button onClick={() => signIn("google", { callbackUrl })}>
+      <Button onClick={() => signIn("google", { callbackUrl: "/" })}>
         Sign in With Google
       </Button>
     </div>
