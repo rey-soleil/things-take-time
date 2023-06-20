f6053bb9ee8c3c7b934e1d4f4e31017e7f649545
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-06-19 21:51:23 -0700

    feat(timeline): make minutesOnScreen bigger if current task gets close to end

--- components/timeline/HorizontalTimeline.tsx
+++ components/timeline/HorizontalTimeline.tsx
@@ -1,4 +1,5 @@
-import { getPercentFromEnd } from "utils/timeline";
+import { useState } from "react";
+import { MINUTES_ON_SCREEN, getPercentFromEnd } from "utils/timeline";
 import HorizontalTickMarks from "./HorizontalTickMarks";
 
 export default function HorizontalTimeline({
@@ -6,15 +7,28 @@ export default function HorizontalTimeline({
 }: {
   startTime: number | undefined;
 }) {
+  const [minutesOnScreen, setMinutesOnScreen] = useState(MINUTES_ON_SCREEN);
+
+  if (
+    startTime &&
+    getPercentFromEnd(startTime, Date.now(), minutesOnScreen) > 75
+  ) {
+    setMinutesOnScreen(minutesOnScreen * 2);
+  }
+
   return (
     <div className="h-7 w-screen bg-gray-300">
-      <HorizontalTickMarks />
-      {!startTime && <div className="absolute right-1/2 h-9 w-1 bg-[#F2F2F2]"></div>}
+      <HorizontalTickMarks minutesOnScreen={minutesOnScreen} />
+      {!startTime && (
+        <div className="absolute right-1/2 h-9 w-1 bg-[#F2F2F2]"></div>
+      )}
       {startTime && (
         <div
           className="absolute right-1/2 h-7 bg-green-500"
           style={{
-            width: `${getPercentFromEnd(startTime, Date.now()) - 50}%`,
+            width: `${
+              getPercentFromEnd(startTime, Date.now(), minutesOnScreen) - 50
+            }%`,
           }}
         ></div>
       )}
--- utils/timeline.ts
+++ utils/timeline.ts
@@ -1,12 +1,15 @@
 // The only constraint on NUM_TICKMARKS is that it should be a multiple of 2.
 const NUM_TICKMARKS = 4;
-const MINUTES_ON_SCREEN = 60;
+export const MINUTES_ON_SCREEN = 60;
 
 // Returns an array of NUM_TICKMARKS + 1 numbers, each of which is the
 // millisecond value of a tick mark.
 // (There are NUM_TICKMARKS + 1 tick marks because one will always be off-screen.)
-function getTickMarksInMilliseconds(nowInMilliseconds: number) {
-  const minutesBetweenTickMarks = MINUTES_ON_SCREEN / NUM_TICKMARKS;
+function getTickMarksInMilliseconds(
+  nowInMilliseconds: number,
+  minutesOnScreen: number
+) {
+  const minutesBetweenTickMarks = minutesOnScreen / NUM_TICKMARKS;
   const millisecondsBetweenTickMarks = convertMinutesToMilliseconds(
     minutesBetweenTickMarks
   );
@@ -37,22 +40,31 @@ function convertTickMarkToLabel(milliseconds: number) {
 }
 
 function simplifyTimeString(timeString: string) {
-  return timeString.slice(0, 4) + timeString.slice(-2, -1).toLowerCase();
+  const colonIndex = timeString.indexOf(":");
+  const hourWithMinutes = timeString.slice(0, colonIndex + 3);
+  const amOrPM = timeString.slice(-2, -1).toLowerCase();
+  return hourWithMinutes + amOrPM;
 }
 
-export function getPercentFromEnd(milliseconds: number, now: number) {
-  const millisecondsOnScreen = convertMinutesToMilliseconds(MINUTES_ON_SCREEN);
+export function getPercentFromEnd(
+  milliseconds: number,
+  now: number,
+  minutesOnScreen: number
+) {
+  const millisecondsOnScreen = convertMinutesToMilliseconds(minutesOnScreen);
   const end = now + millisecondsOnScreen / 2;
   return (100 * (end - milliseconds)) / millisecondsOnScreen;
 }
 
 // Returns an array of {milliseconds, label, percentFromEnd} objects.
-export function getTickMarks(now: number) {
-  return getTickMarksInMilliseconds(now).map((milliseconds) => {
-    return {
-      milliseconds,
-      label: convertTickMarkToLabel(milliseconds),
-      percentFromEnd: getPercentFromEnd(milliseconds, now),
-    };
-  });
+export function getTickMarks(now: number, minutesOnScreen: number) {
+  return getTickMarksInMilliseconds(now, minutesOnScreen).map(
+    (milliseconds) => {
+      return {
+        milliseconds,
+        label: convertTickMarkToLabel(milliseconds),
+        percentFromEnd: getPercentFromEnd(milliseconds, now, minutesOnScreen),
+      };
+    }
+  );
 }
