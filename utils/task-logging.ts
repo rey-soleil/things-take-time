5b635cb23e3e13b398bcb26bd4ea9c3cbd77b99b
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-06-20 12:15:56 -0700

    feat(toast): link to user's calendar

--- utils/task-logging.ts
+++ utils/task-logging.ts
@@ -14,7 +14,7 @@ export function logToGoogleCalendarAndToast(
     task
   );
   if (googleCalendarPromise) {
-    toastGoogleCalendarCompletion(googleCalendarPromise, task);
+    toastGoogleCalendarCompletion(googleCalendarPromise, session, task);
   }
 }
 
