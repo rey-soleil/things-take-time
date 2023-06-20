5a4609386a730fcbf926495999a234b85d4376f8
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-06-19 21:30:26 -0700

    fix(timeline): don't show out of bound tick marks

--- components/timeline/HorizontalTickMarks.tsx
+++ components/timeline/HorizontalTickMarks.tsx
@@ -5,17 +5,20 @@ export default function HorizontalTickMarks() {
 
   return (
     <>
-      {tickMarks.map(({ milliseconds, label, percentFromEnd }) => (
-        <div
-          key={milliseconds}
-          className="absolute flex h-7 flex-col items-center justify-center text-xs font-semibold text-gray-500 md:text-base"
-          style={{
-            right: `${percentFromEnd}%`,
-          }}
-        >
-          <span>{label}</span>
-        </div>
-      ))}
+      {tickMarks.map(({ milliseconds, label, percentFromEnd }) => {
+        if (percentFromEnd < 0 || percentFromEnd > 100) return <></>;
+        return (
+          <div
+            key={milliseconds}
+            className="absolute flex h-7 flex-col items-center justify-center text-xs font-semibold text-gray-500 md:text-base"
+            style={{
+              right: `${percentFromEnd}%`,
+            }}
+          >
+            <span>{label}</span>
+          </div>
+        );
+      })}
     </>
   );
 }
