0848e2c7b00687b1b563460b3e4d5a722cff339e
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-06-23 11:32:00 -0700

    fix(style): reduce margin bottom

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -4,7 +4,7 @@ import { signIn } from "next-auth/react";
 
 export default function SignIn() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center p-5 mt-8">
       <div className="flex flex-col items-center rounded-md border border-solid border-black p-5 [&>*]:p-4">
         <p>
           Things Take Time is a web app for tracking how you spend your time.
