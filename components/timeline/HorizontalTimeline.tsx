7866b589efb603638e37d962d2be10d6687bfdc3
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-06-27 21:26:18 -0700

    feat(timeline): use PastTask for current task

--- components/timeline/HorizontalTimeline.tsx
+++ components/timeline/HorizontalTimeline.tsx
@@ -2,6 +2,7 @@ import { GCalEvent } from "@/app/utils";
 import { Session } from "next-auth";
 import { useEffect, useState } from "react";
 import { getRecentCalendarEvents } from "utils/calendar";
+import { Task } from "utils/tasks";
 import { MINUTES_ON_SCREEN, getPercentFromEnd } from "utils/timeline";
 import HorizontalTickMarks from "./HorizontalTickMarks";
 import PastTask from "./PastTask";
@@ -9,9 +10,11 @@ import PastTask from "./PastTask";
 export default function HorizontalTimeline({
   session,
   startTime,
+  task,
 }: {
   session: Session;
   startTime: number | undefined;
+  task: Task;
 }) {
   const [minutesOnScreen, setMinutesOnScreen] = useState(MINUTES_ON_SCREEN);
   const [events, setEvents] = useState<GCalEvent[]>();
@@ -39,23 +42,26 @@ export default function HorizontalTimeline({
       {!startTime && (
         <div className="absolute right-1/2 h-9 w-1 bg-[#F2F2F2]"></div>
       )}
-      {/* This is the bar for the current task that starts at the center and moves left */}
       {startTime && (
-        <div
-          className="absolute right-1/2 h-7 bg-green-500"
-          style={{
-            width: `${
-              getPercentFromEnd(startTime, Date.now(), minutesOnScreen) - 50
-            }%`,
-          }}
-        ></div>
+        <PastTask
+          summary={task.content}
+          startTime={startTime}
+          minutesOnScreen={minutesOnScreen}
+        />
       )}
       {/* This is all past events in the current window */}
-      {events?.map((event, index) => (
-        <div key={index}>
-          <PastTask event={event} minutesOnScreen={minutesOnScreen} />
-        </div>
-      ))}
+      {events
+        ?.filter((event) => event.start?.dateTime && event.end?.dateTime)
+        .map((event, index) => (
+          <div key={index}>
+            <PastTask
+              summary={event.summary}
+              startTime={event.start!.dateTime}
+              endTime={event.end!.dateTime}
+              minutesOnScreen={minutesOnScreen}
+            />
+          </div>
+        ))}
     </div>
   );
 }
