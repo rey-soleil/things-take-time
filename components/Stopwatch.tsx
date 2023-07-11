5adb24fcba76b64fd1d913cab8742893a425a3e6
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-07-11 09:42:40 -0700

    comment: explain countdown

--- components/Stopwatch.tsx
+++ components/Stopwatch.tsx
@@ -1,5 +1,6 @@
 import { formatAsHHMMSS } from "utils/time";
 
+// TODO: let users enter an amount of time to count down.
 export default function Stopwatch({ milliseconds }: { milliseconds: number }) {
   // TODO: add style
   return (
