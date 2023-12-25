8f8bd5e731896bc2ada46597ebd30bcd41bb6963
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-12-24 18:42:24 -0800

    fix: show correct tab

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,18 +3,20 @@
 import { BarChart } from '@mui/icons-material';
 import ScheduleIcon from '@mui/icons-material/Schedule';
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
-import { useState } from 'react';
 
 export default function NavBar() {
-    const [value, setValue] = useState(0);
+    if (!window) return
+
+    const path = window.location.pathname;
 
     return (
-        <BottomNavigation showLabels className="fixed bottom-0 w-full flex !justify-evenly" value={value}
-            onChange={(event, newValue) => {
-                setValue(newValue);
-            }}>
-            <BottomNavigationAction className='' icon={<ScheduleIcon />} label="Timer" href='/' />
-            <BottomNavigationAction className='' icon={<BarChart />} label="Insights" href='/insights' />
+        <BottomNavigation
+            showLabels
+            className="fixed bottom-0 w-full flex !justify-evenly"
+            value={path}
+        >
+            <BottomNavigationAction className='' icon={<ScheduleIcon />} label="Timer" href='/' value="/" />
+            <BottomNavigationAction className='' icon={<BarChart />} label="Insights" href='/insights' value="/insights" />
         </BottomNavigation>
     )
 }
