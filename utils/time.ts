e42e4e52dce61d29e7ef27f05b5506836caf1b0b
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2024-05-02 15:06:50 -0700

    feat: implement timer feature

--- utils/time.ts
+++ utils/time.ts
@@ -4,10 +4,36 @@
  * @returns The formatted time
  */
 export function formatAsHHMMSS(milliseconds: number) {
+  const { hours, minutes, seconds } = getHoursMinutesSeconds(milliseconds);
+
+  return format(hours, minutes, seconds);
+}
+
+export function prettyFormat(milliseconds: number){
+  const { hours, minutes, seconds } = getHoursMinutesSeconds(milliseconds);
+
+  if(hours > 0 && minutes % 60 === 0 && seconds % 60 === 0){
+    return `${hours} HR`
+  }
+  if(minutes > 0 && seconds % 60 === 0){
+    return `${minutes} MIN`
+  }
+  if(seconds > 0){
+    return `${seconds} SEC`
+  }
+
+  return format(hours, minutes, seconds);
+}
+
+function getHoursMinutesSeconds(milliseconds: number){
   const seconds = Math.floor(milliseconds / 1000);
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
 
+  return { hours, minutes, seconds };
+}
+
+function format(hours: number, minutes: number, seconds: number){
   const formattedHours = hours > 0 ? `${(hours % 24).toString()}:` : "";
   let formattedMinutes = `${(minutes % 60).toString()}`;
   if (hours > 0) formattedMinutes = formattedMinutes.padStart(2, "0");
