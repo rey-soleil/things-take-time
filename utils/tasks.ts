4105ad1a7da2e27b2b03f7664f723b421a3ad064
rey barceló <68834278+rey-soleil@users.noreply.github.com>
2026-09-09 17:32:07 -0700

    Add Todoist scheduling metadata to task type

--- utils/tasks.ts
+++ utils/tasks.ts
@@ -2,4 +2,12 @@ export type Task = {
   id?: string;
   content: string;
   description?: string;
+  priority?: number;
+  day_order?: number;
+  due?: {
+    date: string;
+    is_recurring: boolean;
+    string: string;
+    timezone?: string | null;
+  } | null;
 };
